b0fa35ea7d423de1a83f3f9abf1adf841fc49b2e
Pascal Siewert <pascal.siewert@rwth-aachen.de>
2019-06-12 19:43:48 +0200

    Fix ReturnExpressionNecessaryCoco

--- PureFun/PureFunOof/PureFun-fe/src/main/java/de/simpleproglang/purefun/coco/ReturnExpressionNecessaryCoCo.java
+++ PureFun/PureFunOof/PureFun-fe/src/main/java/de/simpleproglang/purefun/coco/ReturnExpressionNecessaryCoCo.java
@@ -3,27 +3,32 @@ package de.simpleproglang.purefun.coco;
 import de.se_rwth.commons.logging.Log;
 import de.simpleproglang.purefun._ast.ASTFunction;
 import de.simpleproglang.purefun._ast.ASTReturnStatement;
+import de.simpleproglang.purefun._ast.ASTStatement;
 import de.simpleproglang.purefun._cocos.PureFunASTFunctionCoCo;
+import de.simpleproglang.purefun.printer.TypesPrinter;
 
 public class ReturnExpressionNecessaryCoCo implements PureFunASTFunctionCoCo {
 
     @Override
     public void check(ASTFunction node) {
-        node.getBlockStatement().getStatementList().forEach((st) -> {
+        boolean hasReturnStatement = false;
+        String type = TypesPrinter.printType(node.getType());
+
+        for (ASTStatement st : node.getBlockStatement().getStatementList()) {
             if (st instanceof ASTReturnStatement) {
                 ASTReturnStatement returnSt = (ASTReturnStatement) st;
 
-                if (!node.getName().equalsIgnoreCase("Void")) {
-                    Log.error("Missing return statement in fun " + node.getName());
-                }
+                hasReturnStatement = true;
 
-                if (!returnSt.isPresentReturnExpression() && !node.getName().equalsIgnoreCase("Void")) {
+                if (!returnSt.isPresentReturnExpression() && !type.equalsIgnoreCase("Void")) {
                     Log.error("Missing expression after return since return type is not void");
                 }
-
-
             }
-        });
+        }
+
+        if (!hasReturnStatement && !type.equalsIgnoreCase("Void")) {
+            Log.error("Missing return statement in fun " + node.getName());
+        }
     }
 
 
--- PureFun/PureFunOof/PureFun-fe/src/test/java/cocos/ReturnExpressionNecessaryCocoTest.java
+++ PureFun/PureFunOof/PureFun-fe/src/test/java/cocos/ReturnExpressionNecessaryCocoTest.java
@@ -1,15 +1,22 @@
 package cocos;
 
+import de.se_rwth.commons.logging.Log;
 import de.simpleproglang.purefun._ast.ASTModule;
 import de.simpleproglang.purefun._cocos.PureFunCoCoChecker;
 import de.simpleproglang.purefun.coco.ReturnExpressionNecessaryCoCo;
 import lang.AbstractTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class ReturnExpressionNecessaryCocoTest extends AbstractTest {
 
     public static final String COCO_MODELS_ROOT_PATH = "./src/test/resources/cocos/";
+
+    @BeforeAll
+    public static void disableFailQuick() {
+        Log.enableFailQuick(false);
+    }
 
     @ParameterizedTest
     @CsvSource(
